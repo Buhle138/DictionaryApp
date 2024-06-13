ea28d9eb5dc3ba7ed6c81f2842a7ee974a14d045
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-13 19:35:19 +0200

    Initial Commit

---
+++ DictionaryApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DictionaryApp
+//
+//  Created by Buhle Radzilani on 2024/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DictionaryApp/DictionaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryAppApp.swift
+//  DictionaryApp
+//
+//  Created by Buhle Radzilani on 2024/06/13.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
